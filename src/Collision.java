69dc55be047a619fe2004948161a47125572557a
dezhen1125 <d_zhang8@u.pacific.edu>
2021-10-31 23:42:10 -0700

    change collision class

--- src/Collision.java
+++ src/Collision.java
@@ -1,11 +1,11 @@
-//public class Collision {
+public class Collision {
 	/*
 	 * Takes the height, width, and coordinates of the player and 
 	 * another object from their respective Space and uses them to 
 	 * determine if the two objects overlap in checkCollision() and 
 	 * returns true or false based or whether not it does.
 	 */
-//}
+}
 //
 //public void checkCollision (EnemyCar enemycar) {  
 //    int index = -1; // Index of crashed cars  
