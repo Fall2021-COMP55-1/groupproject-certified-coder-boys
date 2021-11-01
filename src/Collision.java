111c82f81704175635c54b5d419d9d79a8cdc6a6
dezhen1125 <d_zhang8@u.pacific.edu>
2021-10-31 23:30:27 -0700

    changed collision and enemycar class, still have error to be fixed

--- src/Collision.java
+++ src/Collision.java
@@ -6,3 +6,11 @@ public class Collision {
 	 * returns true or false based or whether not it does.
 	 */
 }
+//
+//public void checkCollision (EnemyCar enemycar) {  
+//    int index = -1; // Index of crashed cars  
+//    for (int i = 0; i < crash.length; i++) {  
+//        CrashingObject obj = crashing[i];  
+//        if (obj.crashBy(bullet)) { // Determine whether to collide  
+//            index = i; // Record the index of the collided enemy car  
+//            break; 
--- src/EnemyCar.java
+++ src/EnemyCar.java
@@ -1,6 +1,44 @@
+
+import java.util.Random;
+
+
+//public class PlayerCar extends Object implements EnemyCar {
+//   private int speed = 3;
+//   
+//   public EnemyCar(){
+//       this.image = MainGame.PlayerCar;
+//       width = image.getWidth();
+//       height = image.getHeight();
+//       y = -height;          
+//       Random rand = new Random();
+//       x = rand.nextInt(MainGame.WIDTH - width);
+//   }
+//   /** getScore */
+//   @Override
+//   public int getScore() {  
+//       return 5;
+//   }
+//
+//   /** //outofbounds */
+//   @Override
+//   public     boolean outOfBounds() {   
+//       return y>MainGame.HEIGHT;
+//   }
+//
+//   /** move */
+//   @Override
+//   public void step() {   
+//       y += speed;
+//   }
+//
+//}
+   
+
+
 public class EnemyCar {
 	/*
 	 * Sets the enemy’s speed, width, height, filename, and Space.  
 	 * spawn() returns the vehicle for placement in the Game class.
 	 */
+	
 }
