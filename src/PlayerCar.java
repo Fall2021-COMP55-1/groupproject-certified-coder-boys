52d2360649e29ced9c24726f55a0d878ad0de026
Zach <Zach@10.15.165.252>
2021-10-31 21:45:14 -0700

    Needs revisting

--- src/PlayerCar.java
+++ src/PlayerCar.java
@@ -1,18 +1,15 @@
-public class PlayerCar {
-	/*
-	 * Sets the powerup’s speed, width, height, filename, and Space.
-	 */
+public class PlayerCar{
+
+	 // Sets the powerup’s speed, width, height, filename, and Space.
+
 	String fileName; //yet to have image
 	int width;
 	int height;
 	Space pos;
 	
-	public void PlayerCar() {
-		
-	}
-	
-	public getWidth() {
-		
-	}
-	
+	public void playerCar();
+	public int getWidth();
+	public int getHeight();
+	public Space getSpace();
+	public string getFileName();
 }
