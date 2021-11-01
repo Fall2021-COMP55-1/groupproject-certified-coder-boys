1e8bb87b1fa4971bc00b41ddf446589fa949f1f7
Zach <Zach@10.15.165.252>
2021-10-31 21:38:55 -0700

    Possible way to write functions?

--- src/PlayerCar.java
+++ src/PlayerCar.java
@@ -2,13 +2,17 @@ public class PlayerCar {
 	/*
 	 * Sets the powerup’s speed, width, height, filename, and Space.
 	 */
-	String fileName;
+	String fileName; //yet to have image
 	int width;
 	int height;
 	Space pos;
 	
+	public void PlayerCar() {
+		
+	}
 	
-	
-	
+	public getWidth() {
+		
+	}
 	
 }
