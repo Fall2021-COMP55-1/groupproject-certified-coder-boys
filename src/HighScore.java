74f9dd35e41c29124319658c460260df9ca64b2a
wbalbuena <w_balbuena@u.pacific.edu>
2021-11-03 10:34:40 -0700

    HighScore.java
    HighScore.java reads HighScore.txt, and assigns the names and scores to
    their respective arrays and then prints them both in order.

--- src/HighScore.java
+++ src/HighScore.java
@@ -23,10 +23,30 @@ public class HighScore {
 		scores = new int[10];
 		names = new String[10];
 		
-		File f = new File("HighScores.txt");
+		//text file contains all of the scores in a single line
+		File data = new File("HighScores.txt");
 		try {
-			reader = new Scanner(f);
+			reader = new Scanner(data);
 			list = reader.nextLine();
+
+		    //reads text file and assigns the scores/names to their respective arrays
+			for(int i = 0; i<10; i++){
+				names[i] = list.substring(12*i,12*i+6);
+				
+			    String sc = list.substring(12*i+6, 12*i+12);
+			    int numScore = Integer.parseInt(sc);
+
+			    scores[i] = numScore;
+			    
+			    //names[0] = list.substring(0, 6)
+			    //names[1] = list.substring(12, 18)
+			    //names[2] = list.substring(24, 30)
+				
+			    //scores[0] = list.substring(6, 12)
+				//scores[1] = list.substring(18, 24)
+			    //scores[2] = list.substring(30,36)
+			}
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
 		}
@@ -42,6 +62,12 @@ public class HighScore {
 	
 	public static void main(String args[]) {
 		HighScore o = new HighScore();
-		System.out.println(o.list);
+		//System.out.println(o.list);
+		
+		for(int i = 0; i<10; i++) {
+			System.out.println("Name: "+o.names[i]+" Score: "+o.scores[i]);
+		}
+		
+		
 	}
 }
