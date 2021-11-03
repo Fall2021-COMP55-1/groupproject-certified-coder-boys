1bfbec5b5f40ddd37feb7634001eb4399e70d1d2
zreed1 <zreed1@STK-ECS-JR3W243.stk.pacific.edu>
2021-11-03 10:48:18 -0700

    saving changes

--- src/HighScoreMenu.java
+++ src/HighScoreMenu.java
@@ -7,5 +7,19 @@ public class HighScoreMenu {
 	 * and assigns them to their respective arrays of scores[] and names[].  
 	 * Getter methods return the values so it can be properly displayed.
 	 */
+	int scores[];
+	//string names[];
 	
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	//public void HighScoreMenu() {
+		//return scores;
+	//}
+	
+	public void getName() {
+		//return names;
+	}
 }
+//not finished yet
