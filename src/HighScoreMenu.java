4e01a4ebc1faa87d8e5f0d8765e6bffd287f9d40
zreed1 <zreed1@STK-ECS-JR3W243.stk.pacific.edu>
2021-11-03 10:44:27 -0700

    testing adding import

--- src/HighScoreMenu.java
+++ src/HighScoreMenu.java
@@ -1,8 +1,11 @@
+import java.awt.event.MouseEvent;
+
 public class HighScoreMenu {
+	
 	/*
 	 * The constructor gets names and scores in order from a text file 
 	 * and assigns them to their respective arrays of scores[] and names[].  
 	 * Getter methods return the values so it can be properly displayed.
 	 */
-	//test123
+	
 }
