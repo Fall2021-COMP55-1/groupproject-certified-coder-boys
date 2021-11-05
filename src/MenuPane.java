097afbeeb5b32d049756ab0ae56af3396c88f21e
shahbajsohal <89656941+shahbajsohal@users.noreply.github.com>
2021-11-05 10:47:47 -0700

    Added the other buttons to the MenuPane class!

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -10,6 +10,8 @@ public class MenuPane extends GraphicsPane {
 								
 	private GButton rect;
 	private GButton rect2;
+	private GButton rect3;
+	private GButton rect4;
 	private final int BUTTON_SIZE = 50;
 
 	public MenuPane(MainApplication app) {
@@ -17,20 +19,28 @@ public class MenuPane extends GraphicsPane {
 		program = app;
 		rect = new GButton("Next", app.getWidth()/2-BUTTON_SIZE/2, app.getHeight()/2-BUTTON_SIZE/2, BUTTON_SIZE, BUTTON_SIZE);
 		rect2 = new GButton("High Score", 300, 300, BUTTON_SIZE, BUTTON_SIZE);
+		rect3 = new GButton("Play", 200, 200, BUTTON_SIZE, BUTTON_SIZE);
+		rect4 = new GButton("Exit", 400, 400, BUTTON_SIZE, BUTTON_SIZE);
 		rect.setFillColor(Color.RED);
 		rect2.setFillColor(Color.RED);
+		rect3.setFillColor(Color.orange);
+		rect4.setFillColor(Color.yellow);
 	}
 
 	@Override
 	public void showContents() {
 		program.add(rect);
 		program.add(rect2);
+		program.add(rect3);
+		program.add(rect4);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(rect);
 		program.remove(rect2);
+		program.remove(rect3);
+		program.remove(rect4);
 	}
 
 	@Override
