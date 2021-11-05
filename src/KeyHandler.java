12f3fe7459cdb82683f125482b7befcf452e7b0c
shahbajsohal <89656941+shahbajsohal@users.noreply.github.com>
2021-11-04 19:08:21 -0700

    Created KeyHandler Class
    Added KeyHandler class to check for key being pressed while game is
    played.

---
+++ src/KeyHandler.java
@@ -0,0 +1,14 @@
+import java.awt.event.KeyListener;
+
+//public class KeyHandler implements KeyListener {
+//	private boolean[] keys;
+//	private int[] polled;
+//	public KeyHandler() {
+//		keys = new boolean[256];
+//		polled = new int[256];
+//	}
+//	
+//	public boolean keyDown(int keycode) {
+//		return polled[keycode] > 0;
+//	}
+//}
