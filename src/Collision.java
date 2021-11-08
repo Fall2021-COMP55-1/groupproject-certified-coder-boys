e07c8c7b0426e98f2f386a70b7fe8d5fe38533ca
dezhen1125 <d_zhang8@u.pacific.edu>
2021-11-08 09:50:56 -0800

    change collision class

---
+++ src/Collision.java
@@ -0,0 +1,9 @@
+/*
+	 * Takes the height, width, and coordinates of the player and 
+	 * another object from their respective Space and uses them to 
+	 * determine if the two objects overlap in checkCollision() and 
+	 * returns true or false based or whether not it does.
+	 */
+public class Collision {
+
+}
