f6520241a11ad63f08e5219ad92e2bff44cf74ef
wbalbuena <w_balbuena@u.pacific.edu>
2021-11-08 10:36:51 -0800

    PlayPane.java
    Spaced for readability and added more comments from notes.

--- src/PlayPane.java
+++ src/PlayPane.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import acm.graphics.GImage;
 
 public class PlayPane extends GraphicsPane {
+	boolean gameOver = false;
+	
 	// game loop
 	long startTime = System.nanoTime();
 	//long frametick = 1000000000/60;
@@ -10,16 +12,23 @@ public class PlayPane extends GraphicsPane {
 	long lastTick = startTime;
 	long enemyTick = 1000000000/5000; // for enemy spawn and can change this around
 	//long lastFrame = startTime;
+	
 	ArrayList<EnemyCar> enemies;
 	PlayerCar player;
 	GImage background;
-	Powerup PU;
-	//GUI UI; add this to update the game
+	Powerup item;
 	
-	// while (!gameOver){
-		//if(System.nanotime()-startTime > gametick){}
-	//}
-
+	//GUI UI; add this to update the game's labels
+	
+	public void run() {
+		//define all starting states here
+		while (!gameOver){
+//			if(System.nanotime()-startTime > gametick){
+//				//update, priority: player > enemy > ui
+//			}
+		}
+	}
+	
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
