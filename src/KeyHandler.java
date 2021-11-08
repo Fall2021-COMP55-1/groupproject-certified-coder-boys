1acb5e2052eccaaecec38e7057a4a9b094d01083
wbalbuena <w_balbuena@u.pacific.edu>
2021-11-08 10:36:27 -0800

    KeyHandler.java
    Added necessary methods to implement KeyListener.  Added notes, and
    spaced for readability.

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -1,14 +1,35 @@
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-//public class KeyHandler implements KeyListener {
-//	private boolean[] keys;
-//	private int[] polled;
-//	public KeyHandler() {
-//		keys = new boolean[256];
-//		polled = new int[256];
-//	}
-//	
-//	public boolean keyDown(int keycode) {
-//		return polled[keycode] > 0;
-//	}
-//}
+public class KeyHandler implements KeyListener {
+	private boolean[] keys; //keeps track of all possible keypresses
+	private int[] pulled;
+	
+	public KeyHandler() {
+		keys = new boolean[256];
+		pulled = new int[256];
+	}
+	
+	//checks whether it was pressed or not
+	public boolean keyDown(int keycode) {
+		return pulled[keycode] > 0;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
