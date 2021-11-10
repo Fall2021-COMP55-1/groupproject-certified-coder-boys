2c9bdd503f39abab3a75f4bc6513d5f0f2b05a0f
wbalbuena <w_balbuena@u.pacific.edu>
2021-11-09 16:24:39 -0800

    TitleScreenPane.java
    Displays title screen image.

--- src/TitleScreenPane.java
+++ src/TitleScreenPane.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
@@ -12,24 +13,30 @@ public class TitleScreenPane extends GraphicsPane {
 	private GLabel menuOpt1;
 	private GLabel menuOpt2;
 	private GLabel menuOpt3;
-
+	private GImage tScreen;
 
 	public TitleScreenPane(MainApplication app) {
 		this.program = app;
 		//img = new GImage("robot head.jpg", 100, 100);
 		//para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
 		//para.setFont("Arial-24");
-		menuOpt1 = new GLabel("Play Game", 100, 100);
+		
+		tScreen = new GImage("AssetImages/title screen.png", 0,0);
+		menuOpt1 = new GLabel("Play Game", 300, 400);
+		menuOpt1.setColor(Color.white);
+		tScreen.setSize(800,600);
 	}
 
 	//@Override
 	public void showContents() {
+		program.add(tScreen);
 		program.add(menuOpt1);
 		//program.add(para);
 	}
 
 	//@Override
 	public void hideContents() {
+		program.remove(tScreen);
 		//program.remove(img);
 		//program.remove(para);
 		program.remove(menuOpt1);
