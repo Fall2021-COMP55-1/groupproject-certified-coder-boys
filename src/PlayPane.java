f010e9983b68f375c27fe117bdd5671ffb975ebb
shahbajsohal <89656941+shahbajsohal@users.noreply.github.com>
2021-11-19 10:14:17 -0800

    changed the road image

--- src/PlayPane.java
+++ src/PlayPane.java
@@ -71,7 +71,7 @@ public class PlayPane extends GraphicsPane implements KeyListener, ActionListene
 		background = new GImage("AssetImages/ground infinite texture.jpg",0,0);
 		background.setSize(800,600);
 		
-		road = new GImage("AssetImages/vapor road.gif",150,0);
+		road = new GImage("AssetImages/Final Road asset.gif",150,0);
 		road.setSize(500,600);
 
 		pause = new GLabel("PAUSE", 100, 100);
