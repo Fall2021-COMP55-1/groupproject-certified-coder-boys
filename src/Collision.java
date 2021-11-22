aa4bdf1125cb78f7dc455b63ead08385e604d619
dezhen1125 <d_zhang8@u.pacific.edu>
2021-11-21 23:51:39 -0800

    change collision, need to fix it

--- src/Collision.java
+++ src/Collision.java
@@ -6,17 +6,26 @@
 	 */
 
 public class Collision {
-public int x = 5;
+	public int x = 5;
 	  public int y = 5;
 	  public int width = 50;
 	  public int height = 50;
-	  
-	if(PlayerCar.x < EnemyCar.x + EnemyCar.width &&
-			  PlayerCar.x + PlayerCar.width > EnemyCar.x &&
-			  PlayerCar.y < EnemyCar.y + EnemyCar.height &&
-			  PlayerCar.y + PlayerCar.height > EnemyCar.y)
-			{
-			    System.out.println("Collision Detected");
-			}
-	public static final int NULL_FEATURE = Integer.MAX_VALUE;
+    // collision x-axis
+    bool collisionX = PlayerCar.Position.x + PlayerCar.Size.x >= EnemyCar.Position.x &&
+    		EnemyCar.Position.x + EnemyCar.Size.x >= PlayerCar.Position.x;
+    // collision y-axis
+    bool collisionY = PlayerCar.Position.y + PlayerCar.Size.y >= EnemyCar.Position.y &&
+    		EnemyCar.Position.y + EnemyCar.Size.y >= PlayerCar.Position.y;
+    // collision only if on both axes
+    return collisionX && collisionY;
+
+//	if(PlayerCar.x < EnemyCar.x + EnemyCar.width &&
+//			  PlayerCar.x + PlayerCar.width > EnemyCar.x &&
+//			  PlayerCar.y < EnemyCar.y + EnemyCar.height &&
+//			  PlayerCar.y + PlayerCar.height > EnemyCar.y)
+//			{
+//			    System.out.println("Collision Detected");
+//			}
+//	public static final int NULL_FEATURE = Integer.MAX_VALUE;
+//	}
 }
