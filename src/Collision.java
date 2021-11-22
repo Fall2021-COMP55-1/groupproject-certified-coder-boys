73647e386ec5d6dbc981eee4162adc7c48d1b27c
dezhen1125 <d_zhang8@u.pacific.edu>
2021-11-21 23:43:25 -0800

    Change collision, still need to fix it

--- src/Collision.java
+++ src/Collision.java
@@ -4,6 +4,19 @@
 	 * determine if the two objects overlap in checkCollision() and 
 	 * returns true or false based or whether not it does.
 	 */
-public class Collision {
 
+public class Collision {
+public int x = 5;
+	  public int y = 5;
+	  public int width = 50;
+	  public int height = 50;
+	  
+	if(PlayerCar.x < EnemyCar.x + EnemyCar.width &&
+			  PlayerCar.x + PlayerCar.width > EnemyCar.x &&
+			  PlayerCar.y < EnemyCar.y + EnemyCar.height &&
+			  PlayerCar.y + PlayerCar.height > EnemyCar.y)
+			{
+			    System.out.println("Collision Detected");
+			}
+	public static final int NULL_FEATURE = Integer.MAX_VALUE;
 }
