32718832510aefdce9a4a3d29c7310557eacb127
wbalbuena <wbalbuena>
2021-12-05 22:55:18 -0800

    Cleaned MenuPane.java.

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -15,17 +15,14 @@ public class MenuPane extends GraphicsPane {
 	private GButton rect3;
 	private GButton rect4;
 	private GImage menu;
-	private final int BUTTON_SIZE = 50;
 
 	public MenuPane(MainApplication app) {
 		super();
 		program = app;
-		rect = new GButton("Next", app.getWidth()/2-BUTTON_SIZE/2, app.getHeight()/2-BUTTON_SIZE/2, BUTTON_SIZE, BUTTON_SIZE);
 		rect2 = new GButton("", 275, 227, 248, 248);
 		rect3 = new GButton("", 10, 227, 248, 248);
 		rect4 = new GButton("", 540, 227, 248, 248);
 		menu = new GImage("AssetImages/Menu.png",0,0);
-		rect.setFillColor(Color.RED);
 		rect2.setFillColor(Color.RED);
 		rect2.setVisible(false);
 		rect3.setFillColor(Color.orange);
