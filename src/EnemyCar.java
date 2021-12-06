b404ac070013022560672c4d43cc2001e5bcd273
wbalbuena <wbalbuena>
2021-12-05 22:47:08 -0800

    Cleaned up EnemyCar.java and added documentation.

--- src/EnemyCar.java
+++ src/EnemyCar.java
@@ -6,17 +6,13 @@ import javax.swing.*;
 import acm.graphics.GImage;
 
 public class EnemyCar extends Object implements ActionListener{
-	private double speed;
 	private String fileName;
 	private int width = 50;
 	private int height = 100;
-	private double x;
-	private double y;
 	private double dx;
 	private double dy;
 	private Timer enemyTimer = new Timer(1000, this); // timer that can/will be adjusted
 	private MainApplication program;
-	private PlayPane game;
 	private GImage car;
 	
 	public void actionPerformed(ActionEvent e) {
@@ -30,9 +26,10 @@ public class EnemyCar extends Object implements ActionListener{
 		this.dy = Math.abs(dy);
 		if(dy < 1)
 			this.dy=1;
+		
+		//randomly chosen car image
 		Random rand = new Random();
 		int roll = rand.nextInt() % 7; //based on number of cars
-		
 		switch(roll) {
 			case 0:
 				fileName = "AssetImages/Mclaren.png";
@@ -58,7 +55,6 @@ public class EnemyCar extends Object implements ActionListener{
 			case 7:
 				fileName = "AssetImages/Audi.png";
 				break;
-				
 			default:
 				fileName = "AssetImages/Porsche.png";
 				break; 
@@ -68,27 +64,8 @@ public class EnemyCar extends Object implements ActionListener{
 		car = new GImage(fileName,x,y);
 		car.setSize(width, height);
 	}
-
-	public int getWidth() { 
-		return width;
-	}
 	
-	public int getHeight() { 
-		return height;
-	}
-	
-	public double getY() {
-		return car.getY();
-	}
-	
-	public String getFileName() { 
-		return fileName;
-	}
-
-	public GImage getImage() {
-		return car;
-	}
-	
+	//update location
 	void update() {
 		if(dx!=0) {
 			if(dx>0) {
@@ -113,15 +90,38 @@ public class EnemyCar extends Object implements ActionListener{
 		else
 			car.move(dx, dy);	
 	}
+
+	//getters
+	public int getWidth() { 
+		return width;
+	}
+	
+	public int getHeight() { 
+		return height;
+	}
+	
+	public double getY() {
+		return car.getY();
+	}
+	
+	public String getFileName() { 
+		return fileName;
+	}
+
+	public GImage getImage() {
+		return car;
+	}
 	
 	public double getDX() {
 		return dx;
 	}
 	
+	//setter
 	public void setDX(double d) {
 		dx = d;
 	}
 	
+	//show/hide GImage
 	public void show() {
 		program.add(car); 
 	}
